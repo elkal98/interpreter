655bf2d52bbd4874ac2aa166fa4131eb3df0410b
elkal98 <khalid.alhaddad98@gmail.com>
2026-01-28 16:21:10 +0300

    Rewrite the interpreter to support functions, classes and methods

--- lox/interpreter.py
+++ lox/interpreter.py
@@ -1,111 +1,9 @@
-from abc import ABC, abstractmethod
-from .token_type import * 
+from .token_type import *
+from .objects import * 
 
 class Token:
     def __init__(self, lexeme):
         self.lexeme = lexeme
-
-class Expr(ABC):
-    @abstractmethod
-    def accept(self, visitor):
-        pass
-
-class Literal(Expr):
-    def __init__(self, value):
-        self.value = value
-    def accept(self, visitor):
-        return visitor.visit_literal_expr(self)
-
-class Variable(Expr):
-    def __init__(self, name):
-        self.name = name
-    def accept(self, visitor):
-        return visitor.visit_variable_expr(self)
-
-class Assign(Expr):
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
-    def accept(self, visitor):
-        return visitor.visit_assign_expr(self)
-
-class Binary(Expr):
-    def __init__(self, left, operator, right):
-        self.left = left
-        self.operator = operator
-        self.right = right
-    def accept(self, visitor):
-        return visitor.visit_Binary(self)
-
-class Unary(Expr):
-    def __init__(self, operator, right):
-        self.operator = operator
-        self.right = right
-    def accept(self, visitor):
-        return visitor.visit_Unary(self)
-
-class Grouping(Expr):
-    def __init__(self, expression):
-        self.expression = expression
-    def accept(self, visitor):
-        return visitor.visit_Grouping(self)
-
-class Stmt(ABC):
-    @abstractmethod
-    def accept(self, visitor):
-        pass
-
-class VarStatement(Stmt):
-    def __init__(self, name, initializer):
-        self.name = name
-        self.initializer = initializer
-    def accept(self, visitor):
-        return visitor.visit_var_stmt(self)
-
-class PrintStatement(Stmt):
-    def __init__(self, expression):
-        self.expression = expression
-    def accept(self, visitor):
-        return visitor.visit_print_stmt(self)
-
-class ExpressionStatement(Stmt):
-    def __init__(self, expression):
-        self.expression = expression
-    def accept(self, visitor):
-        return visitor.visit_expression_stmt(self)
-
-class Environment:
-    def __init__(self, enclosing = None):
-        self.values = {}
-        self.enclosing = enclosing
-
-    def define(self, name, value):
-        self.values[name] = value
-
-    def get(self, name):
-        if name in self.values:
-            return self.values[name]
-        raise RuntimeError(f"Undefined variable '{name}'.")
-
-    def assign(self, name, value):
-        if name in self.values:
-            self.values[name] = value
-        elif self.enclosing:
-            self.enclosing.assign(name, value)
-        else:
-            raise RuntimeError(f"Undefined variable '{name}'.")
-
-    def get_at(self, distance, name):
-        env = self
-        for _ in range(distance):
-            env = env.enclosing
-        return env.values[name]
-
-    def assign_at(self, distance, name, value):
-        env = self
-        for _ in range(distance):
-            env = env.enclosing
-        env.values[name] = value
 
 class Interpreter:
     def __init__(self):
@@ -113,53 +11,46 @@ class Interpreter:
         self.environment = self.globals
         self.locals = {}
 
+    def interpret(self, statements):
+        for stmt in statements:
+            self.execute(stmt)
+
     def resolve(self, expr, depth):
         self.locals[expr] = depth
 
     def evaluate(self, expr):
         return expr.accept(self)
 
-    def interpret(self, statements):
-        for stmt in statements:
-            stmt.accept(self)
-  
-    def visit_var_stmt(self, stmt):
-        value = None
-        if stmt.initializer is not None:
-            value = self.evaluate(stmt.initializer)
-        self.globals.define(stmt.name.lexeme, value)
-        return None
+    def execute(self, stmt):
+        stmt.accept(self)
 
-    def visit_print_stmt(self, stmt):
-        value = self.evaluate(stmt.expression)
-        print(value)
-
-    def visit_expression_stmt(self, stmt):
-        self.evaluate(stmt.expression)
-
+    def execute_block(self, statements, environment):
+        previous = self.environment
+        try:
+            self.environment = environment
+            for stmt in statements:
+                self.execute(stmt)
+        finally:
+            self.environment = previous
     def visit_literal_expr(self, expr):
         return expr.value
 
-    def visit_variable_expr(self, expr):
-        distance = self.locals.get(expr)
-        if distance is not None:
-            return self.environment.get_at(distance, expr.name.lexeme)
-        return self.globals.get(expr.name.lexeme)
+    def visit_grouping_expr(self, expr):
+        return self.evaluate(expr.expression)
 
-    def visit_assign_expr(self, expr):
-        value = self.evaluate(expr.value)
-        distance = self.locals.get(expr)
-        if distance is not None:
-            self.environment.assign_at(distance, expr.name.lexeme, value)
-        else:
-            self.globals.assign(expr.name.lexeme, value)
-        return value
+    def visit_unary_expr(self, expr):
+        right = self.evaluate(expr.right)
+        t = expr.operator.token_type
+        if t == TokenType.MINUS:
+            return -right
+        elif t == TokenType.BANG:
+            return not self.is_truthy(right)
+        raise RuntimeError(f"Unknown unary operator {t}")
 
-    def visit_Binary(self, expr):
+    def visit_binary_expr(self, expr):
         left = self.evaluate(expr.left)
         right = self.evaluate(expr.right)
         t = expr.operator.token_type
-
         if t == TokenType.PLUS:
             return left + right
         elif t == TokenType.MINUS:
@@ -168,17 +59,125 @@ class Interpreter:
             return left * right
         elif t == TokenType.SLASH:
             return left / right
+        elif t == TokenType.GREATER:
+            return left > right
+        elif t == TokenType.GREATER_EQUAL:
+            return left >= right
+        elif t == TokenType.LESS:
+            return left < right
+        elif t == TokenType.LESS_EQUAL:
+            return left <= right
+        elif t == TokenType.EQUAL_EQUAL:
+            return left == right
+        elif t == TokenType.BANG_EQUAL:
+            return left != right
+        raise RuntimeError(f"Unknown binary operator {t}")
+
+    def visit_variable_expr(self, expr):
+        distance = self.locals.get(expr)
+        if distance is not None:
+            return self.environment.get_at(distance, expr.name)
+        return self.environment.get(expr.name)
+
+    def visit_assign_expr(self, expr):
+        value = self.evaluate(expr.value)
+        distance = self.locals.get(expr)
+        if distance is not None:
+            self.environment.assign_at(distance, expr.name, value)
         else:
-            raise RuntimeError(f"Unknown binary operator {t}")
+            self.environment.assign(expr.name, value)
+        return value
 
-    def visit_Unary(self, expr):
-        right = self.evaluate(expr.right)
-        t = expr.operator.token_type
+    def visit_call_expr(self, expr):
+        callee = self.evaluate(expr.callee)
+        arguments = [self.evaluate(arg) for arg in expr.arguments]
+        if not isinstance(callee, LoxCallable):
+            raise RuntimeError(expr.paren, "Can only call functions and classes.")
+        if len(arguments) != callee.arity():
+            raise RuntimeError(expr.paren, f"Expected {callee.arity()} arguments but got {len(arguments)}.")
+        return callee.call(self, arguments)
 
-        if t == TokenType.MINUS:
-            return -right
-        else:
-            raise RuntimeError(f"Unknown unary operator {t}")
+    def visit_get_expr(self, expr):
+        obj = self.evaluate(expr.object)
+        if isinstance(obj, LoxInstance):
+            return obj.get(expr.name)
+        raise RuntimeError(expr.name, "Only instances have properties.")
 
-    def visit_Grouping(self, expr):
-        return self.evaluate(expr.expression)
+    def visit_set_expr(self, expr):
+        obj = self.evaluate(expr.object)
+        if not isinstance(obj, LoxInstance):
+            raise RuntimeError(expr.name, "Only instances have fields.")
+        value = self.evaluate(expr.value)
+        obj.set(expr.name, value)
+        return value
+
+    def visit_this_expr(self, expr):
+        if expr in self.locals:
+            distance = self.locals[expr]
+            return self.environment.get_at(distance, expr.keyword)
+        return self.environment.get(expr.keyword)
+
+    def visit_var_stmt(self, stmt):
+        value = None
+        if stmt.initializer is not None:
+            value = self.evaluate(stmt.initializer)
+        self.environment.define(stmt.name, value)
+        return None
+
+    def visit_print_stmt(self, stmt):
+        value = self.evaluate(stmt.expression)
+        print(self.stringify(value))
+        return None
+
+    def visit_expression_stmt(self, stmt):
+        self.evaluate(stmt.expression)
+        return None
+
+    def visit_block_stmt(self, stmt):
+        self.execute_block(stmt.statements, Environment(self.environment))
+        return None
+
+    def visit_function_stmt(self, stmt):
+        function = LoxFunction(stmt, self.environment, False)
+        self.environment.define(stmt.name, function)
+        return None
+
+    def visit_class_stmt(self, stmt):
+        superclass = None
+        if stmt.superclass:
+            superclass = self.evaluate(stmt.superclass)
+            if not isinstance(superclass, LoxClass):
+                raise RuntimeError(stmt.superclass.name, "Superclass must be a class.")
+        self.environment.define(stmt.name, None)
+        if stmt.superclass:
+            self.environment = Environment(self.environment)
+            self.environment.define("super", superclass)
+        methods = {}
+        for method in stmt.methods:
+            function = LoxFunction(method, self.environment, method.name.lexeme == "init")
+            methods[method.name.lexeme] = function
+        klass = LoxClass(stmt.name.lexeme, superclass if isinstance(superclass, LoxClass) else None, methods)
+        if stmt.superclass:
+            self.environment = self.environment.enclosing
+        self.environment.assign(stmt.name, klass)
+        return None
+    
+    def visit_return_stmt(self, stmt):
+        value = None
+        if stmt.value is not None:
+            value = self.evaluate(stmt.value)
+        raise Return(value)
+
+    def is_truthy(self, obj):
+        if obj is None:
+            return False
+        if isinstance(obj, bool):
+            return obj
+        return True
+
+    def stringify(self, obj):
+        if obj is None:
+            return "nil"
+        if isinstance(obj, float) and obj.is_integer():
+            return str(int(obj))
+        return str(obj)
